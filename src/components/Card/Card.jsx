e629ed9476d9ef399342773af64d78db0b1a143d
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2024-11-20 00:11:25 +0600

    modal updated

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,47 +1,46 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
 const Card = ({ singleData }) => {
     const { word, pronunciation, meaning, part_of_speech, when_to_say, example, difficulty } = singleData;
     const [isModalOpen, setIsModalOpen] = useState(false);
-
     const toggleModal = () => {
         setIsModalOpen(!isModalOpen);
     };
 
     return (
-        <div className='bg-slate-100 p-3 rounded-md border backdrop-blur-3xl  '>
-            
-            <div
-                className={` rounded-md shadow-md h-56 p-1 backdrop-blur-3xl ${
-                    difficulty === 'easy'
-                        ? 'bg-green-200'
-                        : difficulty === 'medium'
-                        ? 'bg-yellow-200'
-                        : difficulty === 'hard'
-                        ? 'bg-red-200'
-                        : 'bg-gray-100'
-                }`}
-            >
-                <div className="rounded-md px-1 h-full text-center">
-                    <h1 className="pt-8 font-bold font-mono text-xl">Word: {word}</h1>
-                    <p className="font-bold font-mono text-xl">Meaning: {meaning}</p>
-                    <p className="font-bold font-mono text-xl">Pronunciation: {pronunciation}</p>
-                    <p className="font-bold font-mono text-xl">Part of Speech: {part_of_speech}</p>
+        <>
+            <div className='bg-slate-100 p-3 rounded-md border backdrop-blur-3xl'>
+                <div
+                    className={`rounded-md shadow-md h-56 p-1 backdrop-blur-3xl ${
+                        difficulty === 'easy'
+                            ? 'bg-green-200'
+                            : difficulty === 'medium'
+                            ? 'bg-yellow-200'
+                            : difficulty === 'hard'
+                            ? 'bg-red-200'
+                            : 'bg-gray-100'
+                    }`}
+                >
+                    <div className="rounded-md px-1 h-full text-center">
+                        <h1 className="pt-8 font-bold font-mono text-xl">Word: {word}</h1>
+                        <p className="font-bold font-mono text-xl">Meaning: {meaning}</p>
+                        <p className="font-bold font-mono text-xl">Pronunciation: {pronunciation}</p>
+                        <p className="font-bold font-mono text-xl">Part of Speech: {part_of_speech}</p>
 
-                    <div className="flex justify-center mt-2">
-                        <button
-                            onClick={toggleModal}
-                            className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500"
-                        >
-                            <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
-                            <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
-                            <span className="relative text-white">When to Say</span>
-                        </button>
+                        <div className="flex justify-center mt-2">
+                            <button
+                                onClick={toggleModal}
+                                className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500"
+                            >
+                                <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
+                                <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+                                <span className="relative text-white">When to Say</span>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
 
-           
             {isModalOpen && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
                     <div className="bg-white p-6 rounded-lg shadow-lg text-center">
@@ -58,8 +57,7 @@ const Card = ({ singleData }) => {
                     </div>
                 </div>
             )}
-        </div>
+        </>
     );
 };
-
 export default Card;
