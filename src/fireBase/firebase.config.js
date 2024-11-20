39988303733c40ca9915f3413cb2b6e7de6322e8
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2024-11-21 00:21:23 +0600

    Secured Firebase configuration keys using environment variables

--- src/fireBase/firebase.config.js
+++ src/fireBase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDANYsMQJ-0SxvHVeeFCVnA9OQzx2fflh4",
-  authDomain: "lingo-bingo-b40f0.firebaseapp.com",
-  projectId: "lingo-bingo-b40f0",
-  storageBucket: "lingo-bingo-b40f0.firebasestorage.app",
-  messagingSenderId: "646000675341",
-  appId: "1:646000675341:web:ddf529679d12f882aae344"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
